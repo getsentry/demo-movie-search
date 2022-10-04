fbff0503e63ef6cf95498fc9bd72e13b236bd578
Anton Pirker <anton.pirker@sentry.io>
2022-10-04 11:24:52 +0200

    Cleanup

--- run.sh
+++ run.sh
@@ -8,10 +8,11 @@ python -m venv .venv
 source .venv/bin/activate
 
 # Install (or update) requirements
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # run migrations
 ./manage.py migrate
 
 # run development server on localhost:8000
 ./manage.py runserver 0.0.0.0:8000
+#gunicorn movie_search.project.asgi:application -k uvicorn.workers.UvicornWorker
