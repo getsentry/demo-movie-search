efdbe3c88ae14a8fe0a83c5be2c3556dfd889bf8
Anton Pirker <anton.pirker@sentry.io>
2022-10-19 17:03:40 +0200

    Updated sample project to load span processor for sentry sdk.

--- backend/django/movie_search/project/settings.py
+++ backend/django/movie_search/project/settings.py
@@ -193,14 +193,7 @@ REST_FRAMEWORK = {
 # OpenTelemetry Setup
 
 from opentelemetry import trace
-from opentelemetry.sdk.trace import SpanProcessor
-
-class SentrySpanProcessor(SpanProcessor):
-    def on_start(self, span, parent_context=None):
-        print(f"~~~~~ in on_start {span}")
-
-    def on_end(self,span):
-        print(f"~~~~~ in on_end {span}")
+from sentry_sdk.integrations.opentelemetry import SentrySpanProcessor
 
 provider = trace.get_tracer_provider()
 provider.add_span_processor(SentrySpanProcessor())
