2e3d5ee5eff32cfbde105c13bf5d0e00139af36a
Anton Pirker <anton.pirker@sentry.io>
2023-12-12 11:51:02 +0100

    Added locust for load testing

---
+++ backend/django/locustfile.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def hello_world(self):
+        self.client.get("api/")
+        self.client.get("api/shows/")
+        self.client.get("api/persons/")
+
+    @task(3)
+    def view_items(self):
+        for item_id in range(5):
+            self.client.get(f"api/shows/{1234+item_id}")
+
+    @task
+    def search_items(self):
+        self.client.get("api/shows/?q=scorsese")
+        self.client.get("api/shows/6112/")
+
+    def on_start(self):
+        # self.client.post("/login", json={"username":"foo", "password":"bar"})
+        pass
